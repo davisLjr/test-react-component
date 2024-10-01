5a74522380d80b36035224191bcb30475442b45b
davisLjr <davisprojectjr@gmail.com>
2024-10-01 14:12:06 -0300

    fix: se ajusta prettier

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,12 +3,12 @@ export type Sizes = "default" | "small" | "large";
 
 export const sizeToClass = (size: Sizes | "default"): string => {
   switch (size) {
-  case "default":
-    return "";
-  case "large":
-    return "lg";
-  case "small":
-    return "sm";
+    case "default":
+      return "";
+    case "large":
+      return "lg";
+    case "small":
+      return "sm";
   }
 };
 
